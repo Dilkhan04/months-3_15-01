55d2b6170e32dc5b3a538291740e7d7ac371f1b6
Dilkhan04 <dilkhan.04.e@gmail.com>
2022-07-14 20:57:46 +0600

    dz

---
+++ DZ_2/main.js
@@ -0,0 +1,28 @@
+const block = document.querySelector(".block");
+
+let position = 0;
+let topPosition = 0;
+
+const move = () => {
+	if (position <= 360 && topPosition == 0) {
+		position += 16;
+		block.style.left = `${position}px`;
+		setTimeout(move, 300);
+	} else if (position >= 360 && topPosition <= 360) {
+		topPosition += 16;
+		block.style.top = `${topPosition}px`;
+		setTimeout(move, 300);
+	} else if (topPosition >= 360 && position != 0) {
+		position -= 16;
+		block.style.left = `${position}px`;
+		setTimeout(move, 300);
+	} else if (position == 0 && topPosition != 0) {
+		topPosition -= 16;
+		block.style.top = `${topPosition}px`;
+		setTimeout(move, 300);
+	}
+};
+
+move();
+
+
